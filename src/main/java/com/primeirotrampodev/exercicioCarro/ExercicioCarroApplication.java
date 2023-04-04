71ac4c0a5a4cdc3f50f6f283211b3883ea6767f3
Pablo Henrique <pablohen7ique@gmail.com>
2023-04-04 20:56:12 -0300

    Iniciando Exercicio

---
+++ src/main/java/com/primeirotrampodev/exercicioCarro/ExercicioCarroApplication.java
@@ -0,0 +1,13 @@
+package com.primeirotrampodev.exercicioCarro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExercicioCarroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExercicioCarroApplication.class, args);
+	}
+
+}
